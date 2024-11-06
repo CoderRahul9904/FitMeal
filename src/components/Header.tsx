f6ac68ff80d41773e3b6bf3bd340e748fa108845
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-07 00:40:32 +0530

    changes in name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
         <>
             <header className="bg-white shadow-md">
                     <div className="max-w-4xl mx-auto p-4 flex justify-between items-center">
-                        <div className="text-green-600 font-bold text-xl">FIT MEAL</div>
+                        <div className="text-green-600 font-bold text-xl">EXPLORER MEAL</div>
                         <nav className="space-x-4">
                             <a href="#" className="text-gray-700 hover:text-green-600">Home</a>
                             <a href="#" className="text-gray-700 hover:text-green-600">Dashboard</a>
