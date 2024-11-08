33d4d950d16447106902282c243cc1d2fab442e9
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-09 00:15:04 +0530

    updated Hero container

--- src/components/HeroContainer.tsx
+++ src/components/HeroContainer.tsx
@@ -1,10 +1,11 @@
 
 import Header from './Header'
+import SearchRecipeBox from './SearchRecipeBox'
 function HeroContainer() {
   return (
     <div className=' flex flex-col gap-20 w-screen'>
       <Header />
-      
+      <SearchRecipeBox />
     </div>
 
   )
