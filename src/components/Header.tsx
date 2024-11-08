d247e5410f39bb566ec26ac3a093523849b3e61f
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-09 00:14:28 +0530

    added navigation react-router-dom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
-
+import { useNavigate } from "react-router-dom"
 
 function Header() {
+    const navigate=useNavigate()
+    const handleHome=()=>{
+        navigate('/')
+    }
+    const handleDashboard=()=>{
+        navigate('/dashboard')
+    }
+    const handleFeatures=()=>{
+        navigate('/features')
+    }
+    const handleLogin=()=>{
+        navigate('/login')
+    }
     return (
         <>
             <header className="bg-white shadow-md">
                     <div className="max-w-4xl mx-auto p-4 flex justify-between items-center">
                         <div className="text-green-600 font-bold text-xl">EXPLORER MEAL</div>
                         <nav className="space-x-4">
-                            <a href="#" className="text-gray-700 hover:text-green-600">Home</a>
-                            <a href="#" className="text-gray-700 hover:text-green-600">Dashboard</a>
-                            <a href="#" className="text-gray-700 hover:text-green-600">Features</a>
-                            <a href="#" className="text-gray-700 hover:text-green-600">Login</a>
+                            <a onClick={handleHome} className="text-gray-700 hover:text-green-600 cursor-pointer" >Home</a>
+                            <a onClick={handleDashboard} className="text-gray-700 hover:text-green-600 cursor-pointer">Dashboard</a>
+                            <a onClick={handleFeatures} className="text-gray-700 hover:text-green-600 cursor-pointer">Features</a>
+                            <a onClick={handleLogin} className="text-gray-700 hover:text-green-600 cursor-pointer">Login</a>
                         </nav>
                     </div>
                 </header>
