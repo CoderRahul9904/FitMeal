73b21523add748f6d86fb6b5241c92386c54914f
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-09 00:16:54 +0530

    fixed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import Benifits from "../components/Benifits";
 import Categories from "../components/Categories";
 import FeatureRecipes from "../components/FeatureRecipes";
+import Footer from "../components/Footer";
 import HeroContainer from "../components/HeroContainer";
 import WorkingFlow from "../components/WorkingFlow";
 
@@ -8,15 +9,12 @@ import WorkingFlow from "../components/WorkingFlow";
 function HomePage() {
   return (
     <div className="bg-gray-50 text-gray-800">
-      <HeroContainer />
-      <FeatureRecipes />
-
-      <Categories />
-
-      <Benifits />
-
-      <WorkingFlow />
-
+        <HeroContainer />
+        <FeatureRecipes />
+        <Categories />
+        <Benifits />
+        <WorkingFlow />
+        <Footer />
     </div>
   );
 }
