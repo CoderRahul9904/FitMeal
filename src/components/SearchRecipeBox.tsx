53d20e4f44f7e067f51c386e7a997f774c52fa96
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-08 22:47:33 +0530

    added Search Box

---
+++ src/components/SearchRecipeBox.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+function SearchRecipeBox() {
+  return (
+    <section className="flex flex-col items-center justify-center text-center bg-green-100 py-12 px-6">
+        <h1 className="text-3xl md:text-5xl font-bold text-green-600 mb-4">Discover Your Next Healthy Meal!</h1>
+        <p className="text-lg md:text-xl text-gray-700 mb-6">Your Journey to Healthy Eating Starts Here</p>
+        <div className="flex w-full max-w-md">
+          <SearchBar />
+        </div>
+    </section>
+  )
+}
+
+export default SearchRecipeBox
