7c8483ab7d6cee90cbce0e9ba62d46638d92263d
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-11 22:14:40 +0530

    made changes

---
+++ src/components/ShowMoreRecipe.tsx
@@ -0,0 +1,15 @@
+
+import FeatureRecipes from './FeatureRecipes'
+
+function ShowMoreRecipe() {
+  return (
+    <>
+      <div className=' flex flex-col gap-1 justify-center '>
+        <FeatureRecipes />
+        <button className="text-orange-500 border-2 p-2 rounded-lg flex justify-center items-center w-16 font-semibold border-orange-300">Show More</button>
+      </div>
+    </>
+  )
+}
+
+export default ShowMoreRecipe
