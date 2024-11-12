a3c2dab59198e5c245bb08429b8fd8d7f9a0328b
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-12 18:52:39 +0530

    working

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,12 +4,14 @@ import Header from "../components/Header";
 import MealPlan from "../components/MealPlan";
 import Overview from "../components/Overview";
 import SavedRecipe from "../components/SavedRecipe";
+import Info from "./Info";
 
 
 function Dashboard() {
   return (
     <div className="bg-gray-50 min-h-screen text-gray-800 flex flex-col justify-around">
       <Header />
+      <Info />
       <main className="w-screen mx-auto py-8 px-6">
         <Overview />
         <SavedRecipe />
