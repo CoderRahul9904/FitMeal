dce3b23d9d803497167b0f4b6f6378f1625aed01
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-12 18:53:55 +0530

    working

--- src/components/MealPlan.tsx
+++ src/components/MealPlan.tsx
@@ -9,15 +9,15 @@ function MealPlan() {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
               <div className="p-4 bg-gray-100 rounded-lg">
                 <h4 className="font-semibold mb-2">Breakfast</h4>
-                <button className="text-green-500">+ Add meal</button>
+                <button className="text-orange-500">+ Add meal</button>
               </div>
               <div className="p-4 bg-gray-100 rounded-lg">
                 <h4 className="font-semibold mb-2">Lunch</h4>
-                <button className="text-green-500">+ Add meal</button>
+                <button className="text-orange-500">+ Add meal</button>
               </div>
               <div className="p-4 bg-gray-100 rounded-lg">
                 <h4 className="font-semibold mb-2">Dinner</h4>
-                <button className="text-green-500">+ Add meal</button>
+                <button className="text-orange-500">+ Add meal</button>
               </div>
             </div>
           </div>
