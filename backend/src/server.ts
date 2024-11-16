e6395e5f3134b36eb52117553e22a831446c7a2a
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-16 21:01:46 +0530

    server is been created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,8 @@
-const FitMealDB=require("./db")
+const app=require("./app")
+const Recipe=require("./models/Recipe")
+const FitMeal=require("./db")
+app.listen(3000,()=>{
+    console.log("Server is created")
+})
+
+
