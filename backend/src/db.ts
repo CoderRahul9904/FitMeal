791c4f9b70dd3d9a2aceac6c57ab246da316a3c1
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-16 21:01:17 +0530

    done with db setup

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,6 +3,7 @@ require('dotenv').config({ path: '../config.env' });
 console.log(process.env.DB)
 const db=process.env.DB!.replace('<db_password>',process.env.DB_PASS!)
 
+console.log(db)
 const FitMealDb=mongoose.createConnection(db)
 FitMealDb.on('connected',()=>{
     console.log("Yes connected to FitMeal DB")
