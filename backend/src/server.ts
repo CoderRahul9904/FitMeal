f29870d2e0b7cc4f6565beaad1b744c526813ecd
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-19 23:16:53 +0530

    working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,28 @@
+import { ApolloServer, ApolloServerOptionsWithTypeDefs } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+import { resolvers } from "./graphql/resolvers/index"
+import { readFile } from "node:fs/promises"
 const app=require("./app")
-const Recipe=require("./models/Recipe")
-const FitMeal=require("./db")
-app.listen(3000,()=>{
-    console.log("Server is created")
+const FitMealDb=require("./connectionDB/RecipeConnectionDB")
+const UserDb=require("./connectionDB/UserConnectionDB")
+const RecipeModel=require("./models/Recipe")
+const UserModel=require("./models/User")
+require('dotenv').config({ path: '../config.env' });
+
+const port=process.env.PORT! || 3000
+
+
+const startGraphQLServer=async()=>{
+    const typeDefs=await readFile('./graphql/schemas/schema.graphql','utf8')
+    console.log(typeDefs)
+    const server = new ApolloServer({typeDefs,resolvers});
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+    console.log(`🚀  Server ready at: ${url}`)
+}
+
+startGraphQLServer()
+app.listen(port,()=>{
+    console.log(`Server listening at http://localhost:${port}/`)
 })
-
-
-console.log()
