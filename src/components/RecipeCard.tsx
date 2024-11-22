0280e889e568a057d9f4c929847bd09d4a4ef609
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-22 23:50:38 +0530

    main part for displaying recipes

---
+++ src/components/RecipeCard.tsx
@@ -0,0 +1,68 @@
+import { useQuery } from "@apollo/client"
+
+import { getRecipeCard } from "../graphql/queries"
+import { useState } from "react";
+import PaginationBar from "./PaginationBar";
+import { Link } from "react-router-dom";
+
+interface IRecipeCard {
+  _id: string;
+  name: string;
+  image_url: string;
+  cook_time: number;
+  diet: string;
+}
+
+function RecipeCard() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const { loading, error, data } = useQuery(getRecipeCard, {
+    variables: {
+      limit: 9,
+      offset: (currentPage - 1) * 9
+    }
+  })
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+  console.log(data)
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    console.log("Current Page:", page);
+  };
+  return (
+    <section className="py-10 px-6">
+      <h2 className="text-2xl font-semibold text-center mb-8">Featured Recipes</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
+        {data.recipeCard.limitedRecipes.map((recipe: IRecipeCard) => (
+          <div key={recipe._id} className="bg-white shadow-md rounded-lg overflow-hidden">
+            <div className="aspect-w-16 aspect-h-9">
+              <img
+                src={recipe.image_url}
+                alt={recipe.name}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="p-4">
+              <h3 className="font-semibold text-xl mb-2">{recipe.name}</h3>
+              <p className="text-gray-600">Diet: {recipe.diet} | Time: {recipe.cook_time} min</p>
+              {/* <button className="mt-4 text-orange-500 border-2 border-orange-300 hover:text-white hover:bg-orange-400 px-4 py-2 rounded-md w-full">View Recipe</button> */}
+              <Link
+                to={`/Recipes/${recipe._id}`}
+                className="mt-4 text-orange-500 border-2 border-orange-300 hover:text-white hover:bg-orange-400 px-4 py-2 rounded-md w-full text-center block"
+              >
+                View Recipe
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+      <PaginationBar
+        totalPages={data.recipeCard.totalPage}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </section>
+  )
+}
+
+export default RecipeCard
