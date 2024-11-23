2809b1839ab745caaf37a769cde63b4451fb9ff1
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-23 21:55:27 +0530

    diets

---
+++ src/util/diets.ts
@@ -0,0 +1,15 @@
+
+
+export const diets=
+[
+    'Diabetic Friendly',
+    'Eggetarian',
+    'Gluten Free',
+    'High Protein Non Vegetarian',
+    'High Protein Vegetarian',
+    'No Onion No Garlic (Sattvic)',
+    'Non Vegeterian',
+    'Sugar Free Diet',
+    'Vegan',
+    'Vegetarian'
+]
