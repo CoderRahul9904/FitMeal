f26525d3c9202e146a6c94f2b6aaf00a3e829c03
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-23 21:54:47 +0530

    course array

---
+++ src/util/course.ts
@@ -0,0 +1,19 @@
+
+
+
+export const courses =
+    [
+        'Appetizer',
+        'Brunch',
+        'Dessert',
+        'Dinner',
+        'Indian Breakfast',
+        'Lunch',
+        'Main Course',
+        'North Indian Breakfast',
+        'One Pot Dish',
+        'Side Dish',
+        'Snack',
+        'South Indian Breakfast',
+        'World Breakfast'
+    ]
