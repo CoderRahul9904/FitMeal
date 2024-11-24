ce59135cc8502182f8cf369e437b2db8c10f7713
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-24 22:50:45 +0530

    added filtering Logic

--- src/components/CoursesFilter.tsx
+++ src/components/CoursesFilter.tsx
@@ -1,16 +1,27 @@
 
 
+import { SetCourse } from '../redux/slices/RecipesSlice'
 import { courses } from '../util/course'
+import { useDispatch } from 'react-redux'
+
+
+
 
 function Courses() {
-    // onChange={handleFilterChange}
+  const dispatch=useDispatch()
+  const handleClick=(e:any)=>{
+    const course=e.target.value
+    console.log(course)
+    dispatch(SetCourse({course}))
+  }
+  
   return (
     <>
         <label className="block font-semibold">Course</label>
-            <select name="course"  className="w-full md:w-[70%] lg:w-[90%] p-2 border rounded">
+            <select onChange={(e)=>handleClick(e)} name="course"  className="w-full md:w-[70%] lg:w-[90%] p-2 border rounded">
                 <option value="">Select Course</option>
                 {courses.map((course,index)=>(
-                    <option key={index} value={course.toLowerCase()}>{course}</option>
+                    <option  key={index} value={course}>{course}</option>
                 ))}
         </select>
     </>
