c89d823bb0a64f1f5552802b88958a416d764826
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-24 22:51:29 +0530

    added filtering Logic

--- src/components/DietsFilter.tsx
+++ src/components/DietsFilter.tsx
@@ -1,15 +1,27 @@
 
+import { useDispatch } from 'react-redux'
 import { diets } from '../util/diets'
+import { SetDiet } from '../redux/slices/RecipesSlice'
+
+
+
 
 function DietsFilter() {
-    // onChange={handleFilterChange}
+  const dispatch=useDispatch()
+  const handleClick=(e:any)=>{
+    const diet=e.target.value
+    dispatch(SetDiet({diet}))
+  }
+
+
+
   return (
     <>
       <label className="block font-semibold">Diet</label>
-      <select name="diet"  className="w-full md:w-[70%] lg:w-[90%] p-2 border rounded">
+      <select onChange={(e)=>handleClick(e)} name="diet"  className="w-full md:w-[70%] lg:w-[90%] p-2 border rounded">
         <option value="">Select Diet</option>
         {diets.map((diet,index)=>(
-            <option key={index} value={diet.toLowerCase()}>{diet}</option>
+            <option key={index} value={diet}>{diet}</option>
         ))}
       </select>
     </>
