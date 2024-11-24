7b79edfc4e7783821ed0d3023c07a9e3c10e8870
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-24 22:51:16 +0530

    added filtering Logic

--- src/components/CuisineFilters.tsx
+++ src/components/CuisineFilters.tsx
@@ -1,14 +1,26 @@
 
+import { useDispatch } from 'react-redux'
 import { cuisines } from '../util/cuisine'
+import { SetCuisine } from '../redux/slices/RecipesSlice'
+
+
+
+
 
 function Cuisine() {
+
+  const dispatch=useDispatch()
+  const handleClick=(e:any)=>{
+    const cuisine=e.target.value
+    dispatch(SetCuisine({cuisine}))
+  }
   return (
     <>
       <label className="block font-semibold">Cuisine</label>
-      <select name="cuisine" className="w-full md:w-[70%] lg:w-[90%] p-2 border rounded">
+      <select onChange={(e)=> handleClick(e)} name="cuisine" className="w-full md:w-[70%] lg:w-[90%] p-2 border rounded">
         <option value="">Select Cuisine</option>
         {cuisines.map((cuisine,index) =>(
-            <option key={index} value={cuisine.toLowerCase()}>
+            <option key={index} value={cuisine}>
                {cuisine}
             </option>
         ))}
