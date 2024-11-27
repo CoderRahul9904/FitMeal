ff52a283016ae89579b5b60b0687674f234e60dc
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-27 23:16:55 +0530

    Interface added

---
+++ src/interface/filterRecipes.ts
@@ -0,0 +1,11 @@
+
+
+interface RecipeFiltersState {
+    cook_time: { gte: number; lte: number };
+    courses: string | null;
+    cuisine: string | null;
+    diets: string | null;
+    prep_time: { gte: number; lte: number };
+    ingridentName: string | null;
+    name: string | null;
+}
