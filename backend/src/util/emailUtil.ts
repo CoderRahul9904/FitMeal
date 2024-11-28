5aa3fed74eee4cedc0a4059580f04eabdbeaccfb
CoderRahul9904 <mouryarahul543@gmail.com>
2024-11-28 23:38:42 +0530

    added email body

---
+++ backend/src/util/emailUtil.ts
@@ -0,0 +1,5 @@
+
+export const body=(name:string)=>{
+    return `Hi ${name},\n\nWelcome to FitMeal! 🎉\n\nWe’re excited to have you onboard. With FitMeal, you’ll discover personalized recipes, healthy eating tips, and a community focused on achieving fitness goals.\n\nHere are your next steps:\n1. Explore our meal plans tailored to your preferences.\n2. Track your daily nutrition and fitness progress.\n3. Start your journey to better health today!\n4. Share your Recipe's with world!\n\nIf you have any questions, feel free to reach out to us at mouryarahul543@gmail.com.\n\nStay healthy,\nThe FitMeal Team 🥗`;
+    
+}
