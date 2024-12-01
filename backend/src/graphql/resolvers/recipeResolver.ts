6ada90c967379e55d4abf375aa4f8ba84ce074fd
CoderRahul9904 <mouryarahul543@gmail.com>
2024-12-01 23:38:00 +0530

    working on user part

--- backend/src/graphql/resolvers/recipeResolver.ts
+++ backend/src/graphql/resolvers/recipeResolver.ts
@@ -10,7 +10,8 @@ const recipeResolver={
         //     return await Recipe.find()
         // }, 
         greeting:()=> "hello world",
-        recipeCard: async(_root: any,{limit,offset,filterQuery}:PaginationOptions)=>{
+        recipeCard: async(_root: any,{limit,offset,filterQuery}:PaginationOptions,context:any)=>{
+            console.log("user: ",context)
             const {limitedRecipes,totalPage}=await getRecipes(limit,offset,filterQuery)
             return {limitedRecipes,totalPage}
         } 
