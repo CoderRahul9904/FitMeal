73480d4c4daf60ed3f8b2effa5b631ce10d04fd2
CoderRahul9904 <mouryarahul543@gmail.com>
2024-12-01 23:43:00 +0530

    working

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { SetRecipeName } from '../redux/slices/RecipesSlice'
 function SearchBar() {
+    const recipeName=useSelector((state:any)=> state.recipeFilters)
+    console.log("Name in search Bar: ",recipeName)
+    const dispatch=useDispatch()
+    const handleChange=(e: any)=>{
+        const name=e.target.value
+        dispatch(SetRecipeName({name}))
+    }
     return (
         <>
             <input
+                onChange={(e)=>handleChange(e)}
                 type="text"
+                value={recipeName.name}
                 placeholder="Search recipes..."
                 className="flex-grow p-3 rounded-l-lg border-t border-b border-l border-gray-300"
             />
