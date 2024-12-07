0333e78abf53c29d3a473a1fd2492bb28e25ac19
CoderRahul9904 <mouryarahul543@gmail.com>
2024-12-08 00:49:10 +0530

    working

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,9 +1,26 @@
-import { gql} from "@apollo/client"
+import { gql } from "@apollo/client"
 
 
-export const getCookTime=gql`
+export const getCookTime = gql`
   query{
   getRecipes {
     cook_time
   }
 }`
+
+export const getRecipeCard = gql`
+  query RecipeCard($limit: Int,$offset: Int,$filterQuery:DataQuery){
+    recipeCard(limit: $limit,offset:$offset,filterQuery: $filterQuery) {
+      limitedRecipes {
+        name
+        _id
+        cook_time
+        diet
+        image_url
+      },
+      totalPage
+    }
+  }
+`
+
+
