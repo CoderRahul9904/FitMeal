d4f4081cf6a9c78092432dbffe53407cd27b85c8
CoderRahul9904 <mouryarahul543@gmail.com>
2024-12-25 23:35:56 +0530

    done

---
+++ src/components/RightRecipeBox.tsx
@@ -0,0 +1,14 @@
+import IngredientBox from "./IngredientBox"
+import InstructionBox from "./InstructionBox"
+
+
+function RightRecipeBox() {
+    return (
+        <>
+            <IngredientBox />
+            <InstructionBox />
+        </>
+    )
+}
+
+export default RightRecipeBox
