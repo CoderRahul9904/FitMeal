c51db0852c7eebb7a7e0efabac1ce45b4662c7f2
CoderRahul9904 <mouryarahul543@gmail.com>
2025-01-05 23:07:10 +0530

    done

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import recipeFilterReducer from "../slices/RecipesSlice"
+import communityRecipeReducer from "../slices/CommunityRecipeSlice"
 import applyQueryFilterReducer from "../slices/ApplyQuerySlice"
 const store=  configureStore({
     reducer:{
         recipeFilters:recipeFilterReducer,
-        applyQuery: applyQueryFilterReducer
+        applyQuery: applyQueryFilterReducer,
+        CommunityRecipe:communityRecipeReducer
     }
 })
 
