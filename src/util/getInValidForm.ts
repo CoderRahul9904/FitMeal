71e541d2c920f9a045bac9deec2acf54d5bced3c
CoderRahul9904 <mouryarahul543@gmail.com>
2025-01-05 23:08:22 +0530

    done

---
+++ src/util/getInValidForm.ts
@@ -0,0 +1,9 @@
+
+
+export const getInValidForm=(form:string)=>{
+    return form.split(",").map((val:string)=>upperCaseFirstLetter(val))
+}
+
+export const upperCaseFirstLetter=(val:string)=>{
+    return String(val).charAt(0).toUpperCase()+String(val).charAt(1).toUpperCase() + String(val).slice(2);
+}
