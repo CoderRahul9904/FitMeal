05d347fd88f0a7717aa676a891cbf0feefb85469
CoderRahul9904 <mouryarahul543@gmail.com>
2025-01-12 23:48:30 +0530

    done

--- backend/src/controller/refreshTokenController.ts
+++ backend/src/controller/refreshTokenController.ts
@@ -1,20 +1,19 @@
 import jwt, { JwtPayload } from "jsonwebtoken"
-import {jwtDecode} from "jwt-decode";
 import isTokenExpired from "../util/tokenExpiry";
 
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_SECRET; 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export const refreshAccessToken = (req: any, res: any) => {
+export const refreshAccessToken = async(req: any, res: any) => {
     const refreshToken = req.body.refreshToken;
-    console.log(refreshToken)
+    console.log("Here is refresh token",refreshToken)
     if (!refreshToken) return res.status(401).json({ message: "Please login again" });
     try {
         const decoded = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET!) as JwtPayload;
-        if (!isTokenExpired(refreshToken)) return res.status(401).json({ message: "Refresh token expired" });
-
-        const newAccessToken = jwt.sign({ id: decoded.user._id }, JWT_SECRET, { expiresIn: '1h' });
-        console.log(newAccessToken)
+        console.log("ajjajjjajajajjajaj: ", decoded)
+        if (isTokenExpired(refreshToken)) return res.status(401).json({ message: "Refresh token expired" });
+        const newAccessToken =jwt.sign({ id: decoded.id }, JWT_SECRET, { expiresIn: '1h' });
+        console.log("New Token yaha hai",newAccessToken)
 
         return res.status(200).json({ accessToken: newAccessToken });
     } catch (error) {
